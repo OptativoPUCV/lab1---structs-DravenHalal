83d1dc1dd46a3bcbce223fb40fcadf9eeabec751
Your Name <you@example.com>
2024-03-14 15:05:12 +0000

    new try

--- exercises.c
+++ exercises.c
@@ -43,9 +43,10 @@ int *filterEvenNumbers(int arr[], int size, int *newSize) {
   *newSize = 0;
   int *paresArr = (int *)malloc(size * sizeof(int));
   for(int i = 0; i < size; i++){
-    if(arr[i] % 2 == 0)
+    if(arr[i] % 2 == 0){
       paresArr[i] = arr[i];
       newSize++;
+    }
   }
   return paresArr; 
 }
