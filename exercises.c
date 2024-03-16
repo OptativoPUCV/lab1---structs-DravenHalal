fdc08f77d432799cd7fb704e51e9bb1bacbce850
Your Name <you@example.com>
2024-03-16 00:55:22 +0000

    new try

--- exercises.c
+++ exercises.c
@@ -64,6 +64,7 @@ void mergeSortedArrays(int arr1[], int size1, int arr2[], int size2, int result[
     int y = 0;
     int sumSize = size1 + size2;
     for (int i = 0; i < sumSize; i++) {
+      printf("%d ",size1);
         if (y < size1 && (x >= size2 || arr1[y] <= arr2[x])) {;
             result[i] = arr1[y++];
         } 
